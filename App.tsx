402e5d6883fcec33c639d1db855c69e3aa084132
Shahabaz Khan <shahbaz.9676@gmail.com>
2023-08-16 23:11:08 +0530

    First commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+function App(): JSX.Element {
+  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
+}
+
+export default App;
